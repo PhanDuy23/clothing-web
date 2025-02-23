5a82cc78eb64dcec0e3912694f5763988531d43b
PhanDuy23 <bom717323@gmail.com>
2025-02-23 17:17:35 +0700

    fix: deploy github page

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ export default function App() {
 
   return (
    
-      <BrowserRouter>
+      <BrowserRouter basename="/Clothing-web">
         <Routes>
           <Route path="/*" element={<ClientRoutes/>}/>
         </Routes>
